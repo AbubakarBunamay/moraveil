a0d2d15a96c5f86e46589a9a3c1ddb3761491038
Abubakar Bunamay <ab20fx@brocku.ca>
2023-10-02 11:09:00 -0400

    Improved Flashlight Mechanic
    Follows Camera
    Refactored Code

--- Assets/Scripts/FlashLightcontroller.cs
+++ Assets/Scripts/FlashLightcontroller.cs
@@ -7,23 +7,32 @@ public class FlashLightcontroller : MonoBehaviour
 {
     public Light flashLight;
     private bool isFlashlightOn = false;
+    private Transform playerCamera;
 
     // Start is called before the first frame update
     void Start()
     {
         flashLight = GetComponent<Light>();
         flashLight.enabled = false;
+
+        // Find the camera
+        playerCamera = transform.parent;
+
+        // Make the flashlight a child of the camera.
+        transform.SetParent(playerCamera);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //When Right click is placed
         if ( Input.GetMouseButtonDown(1) ) {
             ToggleFlashlight();
         }
 
     }
 
+    //Flashlight Mechanics
     private void ToggleFlashlight ()
     {
         isFlashlightOn = !isFlashlightOn;
