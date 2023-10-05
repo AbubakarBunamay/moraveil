fcbd8b03b2c34497dbe62a0c687edb9f9738e06d
Abubakar Bunamay <ab20fx@brocku.ca>
2023-10-05 17:57:58 -0400

    Stress Manager
    Added mechanic
    No Noise
    Not completed
    ->Darkens screen when in trigger area

---
+++ Assets/Scripts/StressManager.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StressManager : MonoBehaviour
+{
+    public float maxStress = 100f; // Maximum stress level.
+    public float stressIncreaseRate = 10f; // Rate at which stress increases per second.
+    public float stressDecreaseRate = 5f; // Rate at which stress decreases per second.
+    public string playerTag = "Player"; // Tag of the player GameObject.
+    //public Image stressMeterUI; // Reference to the stress meter UI element.
+    public Image darkScreen; // Reference to the full-screen darkening effect.
+
+    private float currentStress = 0f;
+
+    private bool playerInsideTrigger = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            playerInsideTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            playerInsideTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        // Increase stress if the player is inside the trigger zone.
+        if (playerInsideTrigger)
+        {
+            currentStress += stressIncreaseRate * Time.deltaTime;
+        }
+        else
+        {
+            // Decrease stress if the player is outside the trigger zone.
+            currentStress -= stressDecreaseRate * Time.deltaTime;
+        }
+
+        // Ensure stress stays within the 0 to maxStress range.
+        currentStress = Mathf.Clamp(currentStress, 0f, maxStress);
+
+        // Update the stress meter UI.
+        //stressMeterUI.fillAmount = currentStress / maxStress;
+
+        // Toggle the darkening effect based on stress level.
+        if (currentStress > 0)
+        {
+            darkScreen.color = new Color(0f, 0f, 0f, currentStress / maxStress);
+        }
+        else
+        {
+            darkScreen.color = Color.clear;
+        }
+    }
+}
