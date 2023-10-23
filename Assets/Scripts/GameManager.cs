79fba5ebc26819602efad4a8a43faad469555e5c
ab20fx <ab20fx@CFDH-WTH269G08>
2023-10-23 11:55:28 -0400

    Setup GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private bool isGameOver = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartGame()
+    {
+
+    }
+
+    void GameOver()
+    {
+
+    }
+
+     void ResetGame()
+    {
+        
+    }
+}
