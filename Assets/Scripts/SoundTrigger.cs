df78b6ff1d451b9a8485f4a6c7a4ab2351964e06
Abubakar Bunamay <ab20fx@brocku.ca>
2023-11-08 21:53:15 -0500

    Set up SoundTrigger
    When player passes through a sound can be played

--- Assets/Scripts/SoundTrigger.cs
+++ Assets/Scripts/SoundTrigger.cs
@@ -2,6 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class SoundTrigger : MonoBehaviour
+{
+    public AudioClip soundToPlay;      // The audio clip to play when triggered.
+    private AudioSource audioSource;   // Reference to the AudioSource component attached to the GameObject.
+    public bool playOnce = true;       // Should the sound be played only once when triggered?
+
+    private bool hasPlayed = false;    // Tracks whether the sound has been played.
+
+    void Start()
+    {
+        // Get the AudioSource component attached to this GameObject.
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!hasPlayed || !playOnce)
+        {
+            // Check if the object entering the trigger area has a specific tag (in this case, "Player").
+            if (other.CompareTag("Player"))
+            {
+                // Set the AudioClip for the AudioSource and play it.
+                audioSource.clip = soundToPlay;
+                audioSource.Play();
+
+                // Mark that the sound has been played.
+                hasPlayed = true;
+            }
+        }
+    }
+}
+
 //public class SoundTrigger : MonoBehaviour
 //{
 //    public AudioSource audioSource;
