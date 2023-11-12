ad56957d0d18e7787611bf77c119d02a85524693
Abubakar Bunamay <ab20fx@brocku.ca>
2023-11-11 20:25:51 -0500

    Added StressTrigger Script
    This script calls from the stressmanager and can be areas where stress gets trigger or around an object

---
+++ Assets/Scripts/StressTrigger.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StressTrigger : MonoBehaviour
+{
+    public StressManager stressManager;
+    private bool playerInsideTrigger = false; // By default player not inside trigger
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stressManager = FindObjectOfType<StressManager>();
+
+    }
+
+    public void Update()
+    {
+
+        // Increase stress if the player is inside the trigger zone.
+        if (playerInsideTrigger)
+        {
+            // Update stress, trigger stress effects, and update UI.
+            stressManager.StressEffects();
+            stressManager.IncreaseStressTrigger();
+        }
+        else
+        {
+            // Decrease stress continuously if the player is outside the trigger zone.
+            stressManager.ResetStressEffects();
+            stressManager.DecreaseStressTrigger();
+        }
+
+    }
+
+
+    // OnTriggerEnter is called when the Collider other enters the trigger
+    void OnTriggerEnter(Collider other)
+    {
+        if (stressManager != null && other.CompareTag(stressManager.playerTag))
+        {
+            playerInsideTrigger = true;
+        }
+    }
+
+    // OnTriggerExit is called when the Collider other exits the trigger
+    void OnTriggerExit(Collider other)
+    {
+        if (stressManager != null && other.CompareTag(stressManager.playerTag))
+        {
+            playerInsideTrigger = false;
+        }
+    }
+}
