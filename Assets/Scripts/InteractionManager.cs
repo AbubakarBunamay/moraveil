6e7ed24ac80eb9293d52f2efff38625fa1981abe
Abubakar Bunamay <ab20fx@brocku.ca>
2023-11-12 20:54:47 -0500

    Update InteractionManager
    Added symbol raycasting

--- Assets/Scripts/InteractionManager.cs
+++ Assets/Scripts/InteractionManager.cs
@@ -3,24 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InteractionManager : MonoBehaviour{
-    
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+            RaycastHit interactHit;
 
-            //If reaches an object 
-            if (Physics.Raycast(ray, out hit))
+            // Raycast for general interactables
+            if (Physics.Raycast(ray, out interactHit))
             {
-                //Interactable
-                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                Interactable interactable = interactHit.collider.GetComponent<Interactable>();
                 if (interactable != null)
                 {
-                    interactable.Interact(); //Interact with object 
+                    interactable.Interact(); // Interact with general object 
+                }
+            }
+
+            // Raycast for symbols
+            RaycastHit symbolHit;
+            if (RaycastForSymbol(ray, out symbolHit))
+            {
+                SymbolInteract symbol = symbolHit.collider.GetComponent<SymbolInteract>();
+                if (symbol != null)
+                {
+                    symbol.Interact(); // Interact with symbol
                 }
             }
         }
+    }
+
+    private bool RaycastForSymbol(Ray ray, out RaycastHit hit)
+    {
+        int symbolLayer = LayerMask.NameToLayer("Symbol");
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << symbolLayer))
+        {
+            SymbolInteract symbol = hit.collider.GetComponent<SymbolInteract>();
+            if (symbol != null)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
