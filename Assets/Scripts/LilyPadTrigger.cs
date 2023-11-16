f42bb361742349ba971e04573290606ffc3b801e
Abubakar Bunamay <ab20fx@brocku.ca>
2023-11-15 20:49:09 -0500

    LilyPad Setup
    To be completed

---
+++ Assets/Scripts/LilyPadTrigger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LilyPadTrigger : MonoBehaviour
+{
+    public LilySoundManager lilySoundManager;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Play a random sound from the array
+            int randomIndex = Random.Range(0, lilySoundManager.lilySounds.Length);
+            audioSource.PlayOneShot(lilySoundManager.lilySounds[randomIndex]);
+        }
+    }
+}
