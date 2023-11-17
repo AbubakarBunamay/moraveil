3bfdfdb62e0e6e06aae8e5ce69639d0df9c6410b
Abubakar Bunamay <ab20fx@brocku.ca>
2023-11-17 00:56:08 -0500

    Text PopUp Setup
    Updated Player Prefab
    Updated Textmanager to Texttrigger
    Refactored code as a trigger

---
+++ Assets/Scripts/TextPopupTrigger.cs
@@ -0,0 +1,61 @@
+using TMPro;
+using UnityEngine;
+
+public class TextPopupManager : MonoBehaviour
+{
+    public TextMeshProUGUI popupText; // Reference to the TextMeshProUGUI component
+    public string message = "It Works!";
+    private string defaultText = "Default message"; // Default message if no specific message is found
+
+    private void Start()
+    {
+        // Hide the popup text initially
+        HidePopupText();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (IsPlayer(other))
+        {
+            UpdatePopupText();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (IsPlayer(other))
+        {
+            HidePopupText();
+        }
+    }
+
+    private void UpdatePopupText()
+    {
+        // Customize this method to return a specific message or perform actions based on the attached object's logic.
+
+        // Use the default message if no specific message is found
+        SetPopupText(message != null ? message : defaultText);
+    }
+
+    private void SetPopupText(string text)
+    {
+        if (popupText != null)
+        {
+            popupText.text = text;
+            popupText.gameObject.SetActive(true);
+        }
+    }
+
+    private void HidePopupText()
+    {
+        if (popupText != null)
+        {
+            popupText.gameObject.SetActive(false);
+        }
+    }
+
+    private bool IsPlayer(Collider other)
+    {
+        return other.CompareTag("Player");
+    }
+}
