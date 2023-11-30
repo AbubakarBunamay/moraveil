41cdbfef46a132f8e6f481029d29230d5a4740d6
Abubakar Bunamay <ab20fx@brocku.ca>
2023-11-30 15:13:52 -0500

    Refactor Flashlight Controller
    Removed change of color functions

--- Assets/Scripts/Controllers/FlashLightcontroller.cs
+++ Assets/Scripts/Controllers/FlashLightcontroller.cs
@@ -8,22 +8,6 @@ public class FlashLightcontroller : MonoBehaviour
     public Light flashLight;  // Reference to a Light component for the flashlight.
     public bool isFlashlightOn = false;  // A boolean flag to track if the flashlight is on or off.
     private Transform playerCamera;  // Reference to the player's camera.
-
-    public float raycastDistance = 10f;  // The maximum distance for raycasting.
-    public Color targetColor = Color.red;  // The color to change affected objects to.
-    public float colorChangeDuration = 2f;  // The duration it takes to change colors.
-    private float colorChangeStartTime;  // Stores the start time of color change.
-
-
-    private RaycastHit hitInfo;  // Information about what the raycast hits.
-    private List<Renderer> affectedObjects = new List<Renderer>();  // A list to store affected object renderers.
-    private bool isColorChanging = false;  // A flag to track if the color change is in progress.
-    private Dictionary<Renderer, Color> originalColors = new Dictionary<Renderer, Color>();  // Stores the original colors of affected objects.
-    private float remainingColorChangeTime = 0f;  // The remaining time for color change.
-
-    // Store the current coroutine for color change
-    private Coroutine colorChangeCoroutine;
-
     public GlowstickController glowstickController; // Reference to the GlowstickController script.
 
 
@@ -49,25 +33,7 @@ public class FlashLightcontroller : MonoBehaviour
             if (Input.GetMouseButtonDown(1))  // Check if the right mouse button is clicked.
             {
                 ToggleFlashlight();  // Toggle the flashlight on and off.
-
-                if (isFlashlightOn)
-                {
-                    if (!isColorChanging)
-                    {
-                        isColorChanging = true;  // Start color change if not already in progress.
-                        colorChangeCoroutine = StartCoroutine(ChangeColors(remainingColorChangeTime));  // Start the color change coroutine.
-                    }
-                }
-                else
-                {
-                    if (colorChangeCoroutine != null)
-                    {
-                        StopCoroutine(colorChangeCoroutine);  // Stop the color change coroutine.
-                        colorChangeCoroutine = null;
-                        remainingColorChangeTime = colorChangeDuration - (Time.time - colorChangeStartTime);  // Calculate the remaining time for color change.
-                    }
-                    isColorChanging = false;  // Stop color change.
-                }
+                
             }
 
             // Check if the player is underwater and the glowstick controller is available.
@@ -78,22 +44,6 @@ public class FlashLightcontroller : MonoBehaviour
                 flashLight.enabled = false;
                 glowstickController.TurnOnGlowstick();
             }
-
-            if (isFlashlightOn && isColorChanging)
-            {
-                if (Physics.Raycast(playerCamera.position, playerCamera.forward, out hitInfo, raycastDistance))  // Perform a raycast from the camera.
-                {
-                    Renderer rend = hitInfo.collider.GetComponent<Renderer>();  // Get the renderer of the object hit by the ray.
-                    if (rend != null && hitInfo.collider.CompareTag("LightObjects"))  // Check if the hit object has the "LightObjects" tag.
-                    {
-                        if (!affectedObjects.Contains(rend))  // Add the renderer to the list if it's not already in the list.
-                        {
-                            affectedObjects.Add(rend);
-                            originalColors[rend] = rend.material.color;  // Store the original color of the object.
-                        }
-                    }
-                }
-            }
         }
     }
 
@@ -103,23 +53,5 @@ public class FlashLightcontroller : MonoBehaviour
         flashLight.enabled = isFlashlightOn;  // Turn the flashlight on or off.
         Debug.Log("Flashlight is " + (isFlashlightOn ? "on" : "off"));  // Log the flashlight state.
 
-    }
-
-    private IEnumerator ChangeColors(float remainingTime)
-    {
-        float elapsedTime = 0f;  // Initialize the elapsed time.
-        colorChangeStartTime = Time.time - elapsedTime;  // Store the start time of color change.
-
-        while (elapsedTime < colorChangeDuration)  // Continue until the color change duration is reached.
-        {
-            elapsedTime = Time.time - colorChangeStartTime + remainingTime;  // Calculate the elapsed time with remaining time.
-
-            foreach (Renderer rend in affectedObjects)  // Iterate through affected objects.
-            {
-                rend.material.color = Color.Lerp(originalColors[rend], targetColor, elapsedTime / colorChangeDuration);  // Change the color gradually.
-            }
-
-            yield return null;  // Yielding to allow other operations in the frame.
-        }
     }
 }
