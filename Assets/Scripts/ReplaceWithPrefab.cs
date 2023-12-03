b63acc186daa5bd9451ce392fa85c4029839a51c
Abubakar Bunamay <ab20fx@brocku.ca>
2023-12-03 13:32:14 -0500

    Refactor Custom Tools
    Updated ReplaceWithPrefab Tool

--- Assets/Scripts/ReplaceWithPrefab.cs
+++ Assets/Scripts/ReplaceWithPrefab.cs
@@ -1,37 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-
-using UnityEngine;
-using UnityEditor;
 
 public class ReplaceWithPrefab : EditorWindow
 {
+    // Serialized field to store the selected prefab
     [SerializeField] private GameObject prefab;
 
+    // MenuItem attribute to create the menu item "Tools/Replace With Prefab"
     [MenuItem("Tools/Replace With Prefab")]
     static void CreateReplaceWithPrefab()
     {
+        // Open the ReplaceWithPrefab window when the menu item is clicked
         EditorWindow.GetWindow<ReplaceWithPrefab>();
     }
 
+    // GUI function called whenever the editor window is drawn
     private void OnGUI()
     {
+        // Create an ObjectField to select the prefab
         prefab = (GameObject)EditorGUILayout.ObjectField("Prefab", prefab, typeof(GameObject), false);
 
+        // Button to initiate the replacement process
         if (GUILayout.Button("Replace"))
         {
+            // Get the currently selected GameObjects in the hierarchy or scene view
             var selection = Selection.gameObjects;
 
+            // Loop through the selected objects in reverse order
             for (var i = selection.Length - 1; i >= 0; --i)
             {
+                // Get the current selected GameObject
                 var selected = selection[i];
+
+                // Determine the type of the selected prefab
                 var prefabType = PrefabUtility.GetPrefabType(prefab);
                 GameObject newObject;
 
+                // Instantiate the prefab based on its type
                 if (prefabType == PrefabType.Prefab)
                 {
+                    // Instantiate a regular prefab and set its name
                     newObject = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
                 }
                 else
@@ -40,23 +48,32 @@ public class ReplaceWithPrefab : EditorWindow
                     newObject.name = prefab.name;
                 }
 
+                // Check for instantiation errors
                 if (newObject == null)
                 {
                     Debug.LogError("Error instantiating prefab");
                     break;
                 }
 
+                // Register the new object creation for undo functionality
                 Undo.RegisterCreatedObjectUndo(newObject, "Replace With Prefabs");
+
+                // Transfer the transform properties from the selected object to the new object
                 newObject.transform.parent = selected.transform.parent;
                 newObject.transform.localPosition = selected.transform.localPosition;
                 newObject.transform.localRotation = selected.transform.localRotation;
                 newObject.transform.localScale = selected.transform.localScale;
                 newObject.transform.SetSiblingIndex(selected.transform.GetSiblingIndex());
+
+                // Destroy the selected object immediately to complete the replacement
                 Undo.DestroyObjectImmediate(selected);
             }
         }
 
+        // Disable the GUI elements (greyed out)
         GUI.enabled = false;
+
+        // Display the count of selected objects
         EditorGUILayout.LabelField("Selection count: " + Selection.objects.Length);
     }
 }
