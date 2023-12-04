be030ffc7fe5355e5a5a8bb79295719781cf6821
Abubakar Bunamay <ab20fx@brocku.ca>
2023-12-04 14:19:13 -0500

    Refactored Interactable & MapInteraction
    Commented to explain

--- Assets/Scripts/MapInteraction.cs
+++ Assets/Scripts/MapInteraction.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class MapInteraction : MonoBehaviour
 {
-
+    // Interact with the map function 
     public void InteractWithMap()
     {
         Disappear();
     }
 
+    // Map Disappear 
     private void Disappear()
     {
         gameObject.SetActive(false);
