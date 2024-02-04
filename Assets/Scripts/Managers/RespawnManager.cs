4219415e4d1a340d9a7f43ae950ec9da1b1f3f3b
Abubakar Bunamay <ab20fx@brocku.ca>
2024-02-04 14:32:01 -0500

    Setting respawn Manager

--- Assets/Scripts/Managers/RespawnManager.cs
+++ Assets/Scripts/Managers/RespawnManager.cs
@@ -8,6 +8,9 @@ public class RespawnManager : MonoBehaviour
     public Transform  respawnPoint; // The point where the player will respawn.
     public GameObject player; // Reference to the player GameObject.
     
+    public UIManager uiManager; // Reference to the UIManager script.
+    public GameManager gameManager;
+    
     // Public method to initiate player respawn
     public void Respawn()
     {
@@ -24,8 +27,20 @@ public class RespawnManager : MonoBehaviour
             player.transform.position = respawnPoint.position;
             player.transform.rotation = respawnPoint.rotation;
             
+            // Restart the timer in UIManager
+            if (uiManager != null)
+            {
+                gameManager.ResetAndStartTimer();
+                // Hide the restart menu
+                uiManager.RespawnUIReset();
+            }
+            else
+            {
+                Debug.LogError("UIManager script not assigned in the RespawnManager script.");
+            }
+            
             // Log a message with respawned position and rotation
-            Debug.Log("Respawned at position: " + transform.position + ", rotation: " + transform.rotation.eulerAngles);
+            //Debug.Log("Respawned at position: " + transform.position + ", rotation: " + transform.rotation.eulerAngles);
 
         }
         else
