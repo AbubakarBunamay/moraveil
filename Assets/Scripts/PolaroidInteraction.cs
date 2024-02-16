8b0f1b96add02ab8558fe5e573772140f4532640
Abubakar Bunamay <ab20fx@brocku.ca>
2024-02-16 16:07:48 -0500

    Polaroid Animation Init
    Triggering Polaroid Animation

--- Assets/Scripts/PolaroidInteraction.cs
+++ Assets/Scripts/PolaroidInteraction.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class PolaroidInteraction : MonoBehaviour
 {
     private Animator animator;
-    
-    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the Animator component attached to the same GameObject
+        animator = GetComponent<Animator>();
+
+        // Check if animator is not null
+        if (animator == null)
+        {
+            Debug.LogError("Animator component not found on Polaroid!");
+        }
+    }
 
     public void PolaroidInteract()
     {
-        animator.SetTrigger("Polaroid_Animation");
+        // Trigger Polaroid Interaction Animation
+        animator.SetTrigger("polaroidInteraction");
+            
     }
 }
