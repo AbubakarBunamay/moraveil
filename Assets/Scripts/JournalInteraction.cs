0faebfc4dc5756464aefdc3f49d71ec345832902
Abubakar Bunamay <ab20fx@brocku.ca>
2024-02-16 17:42:12 -0500

    Journal Animation Interaction Init

--- Assets/Scripts/JournalInteraction.cs
+++ Assets/Scripts/JournalInteraction.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class JournalInteraction : MonoBehaviour
 {
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the Animator component attached to the same GameObject
+        animator = GetComponent<Animator>();
+
+        // Check if animator is not null
+        if (animator == null)
+        {
+            Debug.LogError("Animator component not found on Journal!");
+        }
+    }
     public void JournalInteract()
     {
-        
+        // Trigger Journal Interaction Animation
+        animator.SetTrigger("journal_Interaction");
     }
 }
