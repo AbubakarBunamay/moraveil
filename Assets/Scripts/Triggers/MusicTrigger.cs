685138b23b51c4d9f5e858f5375f50b3a36e98d3
Abubakar Bunamay <ab20fx@brocku.ca>
2024-02-18 22:30:12 -0500

    Mural Music Replace to dialogues and triggers reversed
    Setup music triggers when close to a mural and play when in trigger, when interact play the dialogue.

---
+++ Assets/Scripts/Triggers/MusicTrigger.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTrigger : MonoBehaviour
+{
+    public AudioClip musicClip; // The audio clip to play
+
+    private AudioSource audioSource; // Reference to the AudioSource component
+
+    private void Start()
+    {
+        // Create an AudioSource for this GameObject
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = musicClip;
+        audioSource.loop = true;
+    }
+
+    // Called when another Collider enters the trigger zone
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the GameObject entering the trigger has a tag "Player"
+        if (other.CompareTag("Player"))
+        {
+            // Play the music clip
+            audioSource.Play();
+        }
+    }
+
+    // Called when another Collider exits the trigger zone
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if the GameObject exiting the trigger has a tag "Player"
+        if (other.CompareTag("Player"))
+        {
+            // Stop the music clip
+            audioSource.Stop();
+        }
+    }
+}
