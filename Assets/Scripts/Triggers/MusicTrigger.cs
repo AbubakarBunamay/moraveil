8758bd5148396f6cf6ce2b4dd7fc77aed7a23952
Abubakar Bunamay <ab20fx@brocku.ca>
2024-03-02 00:41:23 -0500

    Refactored Reference MusicTrigger

--- Assets/Scripts/Triggers/MusicTrigger.cs
+++ Assets/Scripts/Triggers/MusicTrigger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MusicTrigger : MonoBehaviour
 {
-    public AudioClip musicClip; // The audio clip to play
+    [SerializeField] private AudioClip musicClip; // The audio clip to play
 
     private AudioSource audioSource; // Reference to the AudioSource component
 
