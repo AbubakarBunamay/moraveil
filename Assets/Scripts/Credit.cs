d14e53e633ec156b00e41f50d15896c33de05130
Abubakar Bunamay <ab20fx@brocku.ca>
2024-03-17 01:03:00 -0400

    Fixed Credit Screen
    Auto plays the animation now

---
+++ Assets/Scripts/Credit.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f; // Returns Time scale back 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
