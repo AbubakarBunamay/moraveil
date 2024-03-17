26f05eb5c303e35ed632f2df4912fa7f082b8dc1
Abubakar Bunamay <ab20fx@brocku.ca>
2024-03-17 01:26:01 -0400

    When Credits end change to Start Scene
    Using unity event to set the trigger when scene ends to load the next scene.

--- Assets/Scripts/Credit.cs
+++ Assets/Scripts/Credit.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Credit : MonoBehaviour
 {
+    [SerializeField] private Animator creditsAnimation;     // Animation component reference here
+
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 1f; // Returns Time scale back 
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    // Method to load the next scene
+    public void LoadNextScene()
     {
-        
+        SceneManager.LoadScene("StartScene");
     }
+    
 }
