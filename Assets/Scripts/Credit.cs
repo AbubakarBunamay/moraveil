5a5b9ac0e7418f3d4a4365148284dd9e1d8abec2
Abubakar Bunamay <ab20fx@brocku.ca>
2024-04-04 16:15:06 -0400

    Unpause to Play Credit Scene Audio
    Re enabled to unpause the audio listener

--- Assets/Scripts/Credit.cs
+++ Assets/Scripts/Credit.cs
@@ -10,6 +10,8 @@ public class Credit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Unpause all audio
+        AudioListener.pause = false;
         Time.timeScale = 1f; // Returns Time scale back 
     }
     
